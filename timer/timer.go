4188ae793876e02a85a0e632f94fb53bc3347596
Christoph Portmann <c.portmann@salamworld.com>
2014-12-11 09:56:06 +0200

    add accuracy, improve API

--- timer/timer.go
+++ timer/timer.go
@@ -5,34 +5,54 @@ import (
 	"time"
 )
 
+const (
+	Micros = 1000
+	Millis = 1000000
+)
+
 // Timer is a simple timer that prints elapsed time to console or returns it as nanoseconds
 type Timer struct {
 	lastCheckpoint time.Time
+	accuracyFactor int64
 }
 
-// NewAndStart returns a new timer and starts it immediately.
-func NewAndStart() *Timer {
+// NewTimerAndStart returns a new timer and starts it immediately.
+func NewTimer() *Timer {
 	t := Timer{}
-	t.Start()
+	t.accuracyFactor = 1
+	t.Restart()
+	return &t
+}
+
+// NewTimerAndStart returns a new timer and starts it immediately.
+func NewTimerWithAccuracy(accuracy time.Duration) *Timer {
+	t := Timer{}
+	t.SetAccuracy(accuracy)
+	t.Restart()
 	return &t
 }
 
 // Start starts the timer, if it has already been started, the timer will restart
-func (t *Timer) Start() {
+func (t *Timer) Restart() {
 	t.lastCheckpoint = timeNow()
+}
+
+// Start starts the timer, if it has already been started, the timer will restart
+func (t *Timer) SetAccuracy(accuracy time.Duration) {
+	t.accuracyFactor = int64(accuracy)
 }
 
 // LogAndRestart logs the elapsed time since last start and restarts the timer
 func (t *Timer) GetAndRestart() int64 {
 	elapsed := timeNow().Sub(t.lastCheckpoint)
 	t.lastCheckpoint = timeNow()
-	return elapsed.Nanoseconds()
+	return elapsed.Nanoseconds() / t.accuracyFactor
 }
 
 //LogAndContinue logs the elapsed time since last start. The timer will continue.
 func (t *Timer) GetAndContinue() int64 {
 	elapsed := timeNow().Sub(t.lastCheckpoint)
-	return elapsed.Nanoseconds()
+	return elapsed.Nanoseconds() / t.accuracyFactor
 }
 
 // LogAndRestart logs the elapsed time since last start and restarts the timer
--- timer/timer_test.go
+++ timer/timer_test.go
@@ -14,10 +14,19 @@ func TestTimer(t *testing.T) {
 			return currentTimeInTest
 		}
 
-		Convey("It should return 5, when time elapsed by 5 nanoseconds", func() {
-			timerUnderTest := Timer{}
+		Convey("NewTimer should start the returned timer", func() {
+			timerUnderTest := NewTimer()
 
-			timerUnderTest.Start()
+			currentTimeInTest = currentTimeInTest.Add(5 * time.Nanosecond)
+			measuredTimeAfter5 := timerUnderTest.GetAndRestart()
+
+			So(measuredTimeAfter5, ShouldEqual, 5)
+		})
+
+		Convey("It should return 5, when time elapsed by 5 nanoseconds", func() {
+			timerUnderTest := NewTimer()
+
+			timerUnderTest.Restart()
 			currentTimeInTest = currentTimeInTest.Add(5 * time.Nanosecond)
 			measuredTimeAfter5 := timerUnderTest.GetAndContinue()
 
@@ -25,9 +34,9 @@ func TestTimer(t *testing.T) {
 		})
 
 		Convey("It should first return 3, then 5 when GetAndContinue is called after 3 and then after 2 nanoseconds", func() {
-			timerUnderTest := Timer{}
+			timerUnderTest := NewTimer()
 
-			timerUnderTest.Start()
+			timerUnderTest.Restart()
 			currentTimeInTest = currentTimeInTest.Add(2 * time.Nanosecond)
 			measuredTimeAfter2 := timerUnderTest.GetAndContinue()
 			currentTimeInTest = currentTimeInTest.Add(3 * time.Nanosecond)
@@ -38,9 +47,9 @@ func TestTimer(t *testing.T) {
 		})
 
 		Convey("It should first return 3, then 2 when GetAndRestart is called after 3 and then after 2 nanoseconds", func() {
-			timerUnderTest := Timer{}
+			timerUnderTest := NewTimer()
 
-			timerUnderTest.Start()
+			timerUnderTest.Restart()
 			currentTimeInTest = currentTimeInTest.Add(2 * time.Nanosecond)
 			measuredTimeAfter2 := timerUnderTest.GetAndRestart()
 			currentTimeInTest = currentTimeInTest.Add(3 * time.Nanosecond)
@@ -50,25 +59,33 @@ func TestTimer(t *testing.T) {
 			So(measuredTimeAfter3, ShouldEqual, 3)
 		})
 
-		Convey("NewAndStart should start the returned timer", func() {
-			timerUnderTest := NewAndStart()
+		Convey("When accuracy is set to microseconds, timer should return 7 when 7ms are elapsed", func() {
+			timerUnderTest := NewTimer()
+			timerUnderTest.SetAccuracy(time.Millisecond)
+			currentTimeInTest = currentTimeInTest.Add(7 * time.Millisecond)
+			measuredTime := timerUnderTest.GetAndRestart()
 
-			timerUnderTest.Start()
-			currentTimeInTest = currentTimeInTest.Add(5 * time.Nanosecond)
-			measuredTimeAfter5 := timerUnderTest.GetAndRestart()
+			So(measuredTime, ShouldEqual, 7)
+		})
 
-			So(measuredTimeAfter5, ShouldEqual, 5)
+		Convey("When accuracy is set to seconds, timer should return 13 when 13s are elapsed", func() {
+			timerUnderTest := NewTimer()
+			timerUnderTest.SetAccuracy(time.Second)
+			currentTimeInTest = currentTimeInTest.Add(13 * time.Second)
+			measuredTime := timerUnderTest.GetAndRestart()
+
+			So(measuredTime, ShouldEqual, 13)
 		})
 
 		timeNow = timeNowOriginal
 
 		Convey("Timer should return around 100ms +/- 5ms after sleeping for 500ms should", func() {
-			timerUnderTest := NewAndStart()
+			timerUnderTest := NewTimer()
 			time.Sleep(100 * time.Millisecond)
 
-			measuredTimeAfter100millis := timerUnderTest.GetAndRestart()
+			measuredTime := timerUnderTest.GetAndRestart()
 			factor := 1000000
-			So(measuredTimeAfter100millis, ShouldBeBetween, 95*factor, 105*factor)
+			So(measuredTime, ShouldBeBetween, 95*factor, 105*factor)
 		})
 
 	})
